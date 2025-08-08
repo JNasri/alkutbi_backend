90e3079843bee313ab48af13b02b4f54f143f6a2
JNasri <91850229+JNasri@users.noreply.github.com>
2025-08-09 01:04:21 +0300

    change allowedOrigin to include render frontend build

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,9 @@
 const allowedOrigins = [
-  "http://localhost:3000",
-  "http://localhost:5173",
+  // "http://localhost:3000",
+  // "http://localhost:5173",
   // ADD THE PORDUCTION URL HERE
   // "https://your-production-url.com",
+  "https://alkutbi-frontend.onrender.com/",
 ];
 
 module.exports = allowedOrigins;
