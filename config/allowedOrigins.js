586e64650900b355746bae984736115d7bedf7e0
JNasri <91850229+JNasri@users.noreply.github.com>
2026-01-25 22:58:54 +0300

    updated env configurations

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,12 +1,5 @@
-const allowedOrigins = [
-  // "http://localhost:3000",
-  // "http://localhost:5173",
-  // ADD THE PORDUCTION URL HERE
-  // "https://your-production-url.com",
-  "https://alkutbi-frontend.onrender.com",
-  "http://alkutbi-frontend-build.s3-website.me-south-1.amazonaws.com",
-  "https://alkutbigroup.click",
-  "https://www.alkutbigroup.click",
-];
+const allowedOrigins = process.env.FRONTEND_URLS
+  ? process.env.FRONTEND_URLS.split(",")
+  : [];
 
 module.exports = allowedOrigins;
--- server.js
+++ server.js
@@ -46,6 +46,7 @@ mongoose.connection.once("open", () => {
   console.log("Connected to MongoDB!");
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}!`);
+
   });
 });
 
